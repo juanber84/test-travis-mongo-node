271333077c1d869e7a654dbf6981851d24119803
juanber84 <juanber84@gmail.com>
2017-03-31 19:19:55 +0200

    Example test connect mongodb

--- test/index.js
+++ test/index.js
@@ -1,7 +1,24 @@
 'use strict'
 
-describe("Example describe", function () {
-    it("Example spec", function () {
+const mocha = require('mocha')
+const coMocha = require('co-mocha')
+const Promise = require('bluebird')
+const chai = require('chai')
+const assert = chai.assert
 
+coMocha(mocha)
+
+describe("Example describe", function () {
+
+    it("Example spec", function* () {
+        var MongoClient = require('mongodb').MongoClient;
+        let result = yield new Promise(function (resolve, reject) {
+            MongoClient.connect("mongodb://localhost:27017/exampleDb", function (err, db) {
+                if (err) reject(err)
+                resolve('We are connected')
+            })
+        })
+        assert.equal(result, 'We are connected')
     })
+
 })
